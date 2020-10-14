1c994162d34dcd01234676b55d9c72ace933781d
Gisele Galaburri <gisele.galaburri89@gmail.com>
2020-10-14 18:51:53 -0300

    Error Servicio

---
+++ src/main/java/com/foroyoteambien/foro/errores/ErrorServicio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.foroyoteambien.foro.errores;
+
+/**
+ * 
+ * @author Gisele Galaburri <gisele.galaburri89 at gmail.com>
+ */
+public class ErrorServicio extends Exception{
+    
+        public ErrorServicio(String msg) {
+        super(msg);
+    }
+
+}
